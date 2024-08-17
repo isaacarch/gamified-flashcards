01983630aad4f8bc7ea66223740197b3d0ea2283
Isaac C <isaacchris112@gmail.com>
2024-08-17 19:17:15 +0100

    Basic flashcard database setup with upload functionality

--- flashcard_database.py
+++ flashcard_database.py
@@ -1,1 +1,30 @@
 # use SQLite
+# datetime is stored as string
+
+import sqlite3
+
+# Connect to / create flashcard database
+conn = sqlite3.connect('flashcards.db')
+cursor = conn.cursor()
+
+# Create cards table
+cursor.execute('''CREATE TABLE IF NOT EXISTS myCards
+             (front text, back text, score int, last_seen text)''')
+
+# Insert a row of data
+# cursor.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+# Save (commit) the changes
+# conn.commit()
+
+# Close the connection
+conn.close()
+
+    
+def uploadCard(card):
+    atts = card.get_atts()
+    conn = sqlite3.connect('flashcards.db')
+    cursor = conn.cursor()
+    cursor.execute(f"INSERT INTO myCards VALUES {atts}")
+    conn.commit()
+    conn.close()
--- flashcard_functions.py
+++ flashcard_functions.py
@@ -1,4 +1,5 @@
 import datetime
+from flashcard_database import uploadCard
 
 # Scores:
 # 0 -- no knowledge 
@@ -7,28 +8,41 @@ import datetime
 # 3 -- great knowledge 
 
 # temp array to store -- database later
-flashcards = []
+flashcardsArr = []
 
 # flashcard class
 class flashcard:
     def __init__(self, front, back):
-        self.front = front
-        self.back = back
-        self.score = 0
-        self.last_studied = datetime.datetime.now()
+        self.__front = front
+        self.__back = back
+        self.__score = 0
+        self.__last_seen = datetime.datetime.now()
     def __str__(self):
-        return "Front: "+self.front+"\nBack: "+self.back+"\nScore: "+str(self.score)+"\nLast studied: "+str(self.last_studied)
+        return "Front: "+self.__front+"\nBack: "+self.__back+"\nScore: "+str(self.__score)+"\nLast studied: "+str(self.__last_seen)
     def test(self):
-        print("Question: ",self.front)
+        print("Question: ",self.__front)
         input("Answer: ")
         score = input("On a scale of 0 to 3, with 0 being fully incorrect and 3 being fully correct, how accurate was your answer?\n")
         while score.isnumeric == False or 0 < int(score) or 3 > int(score):
             score = input("Please enter 0, 1, 2, or 3: ")
-        self.last_studied = datetime.datetime.now()
+        self.__last_seen = datetime.datetime.now()
+    def get_atts(self):
+        return (self.__front, self.__back, self.__score, str(self.__last_seen))
 
 # create a new flashcard from user-entered text
 def newFlashcard():
     front = input("Question: ")
     back = input("Answer: ")
-    flashcards.append(flashcard(front, back))
-    print(flashcards[-1])
+    flashcardsArr.append(flashcard(front, back))
+    print(flashcardsArr[-1])
+
+def uploadFlashcards():
+    for card in flashcardsArr:
+        print("Uploading...")
+        uploadCard(card)
+    print("All done :)")
+
+newFlashcard()
+newFlashcard()
+newFlashcard()
+uploadFlashcards()
