44fa1b7d4b91e9127106603c8e06cf6a564d5117
Isaac C <isaacchris112@gmail.com>
2024-08-17 21:16:37 +0100

    sort algorithm introduces some randomness

--- flashcard_algorithm.py
+++ flashcard_algorithm.py
@@ -1,7 +1,21 @@
 from flashcard_functions import getCards
-from datetime import timedelta
+from datetime import timedelta, datetime
+import random
 
 flashcards = getCards()
+
+def roundTime(time):
+    timeString = str(time)[0:16]
+    offsetMins = 0
+    timeMinOne = int(timeString[-1])
+    if timeMinOne < 3:
+        offsetMins = 0
+    elif timeMinOne < 8:
+        offsetMins = 5
+    else:
+        offsetMins = 10
+    timeString = str(time)[0:15] + "0:00.000000"
+    return datetime.strptime(timeString, '%Y-%m-%d %H:%M:%S.%f') + timedelta(minutes = offsetMins)
 
 # score is positive integer
 # 0: 5 minutes
@@ -15,7 +29,12 @@ def whenToStudy(card):
     (score, last_seen) = card.get_score_time()
     return last_seen + timedelta(minutes = score*10)
 
+
+
 # sort cards by time to study
 def sortCards():
-    flashcards.sort(key=lambda x: whenToStudy(x))
+    flashcards.sort(key=lambda x: (roundTime(whenToStudy(x)), random.random()))
 
+sortCards()
+for card in flashcards:
+    print(card)
