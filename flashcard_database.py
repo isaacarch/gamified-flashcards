12f2291995bd2db8f34644069bbfa5d2bb4b4c2f
Isaac C <isaacchris112@gmail.com>
2024-08-17 18:47:10 +0100

    realised we need mysql for leaderboard and sqlite for flashcards

--- flashcard_database.py
+++ flashcard_database.py
@@ -1,10 +1,1 @@
-import mysql.connector
-import database_login as login
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user= login.username,
-  password=login.password
-)
-
-print(mydb)
+# use SQLite
---
+++ leaderboard_database.py
@@ -0,0 +1,12 @@
+# use MySQL
+
+import mysql.connector
+import database_login as login
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user= login.username,
+  password=login.password
+)
+
+mycursor = mydb.cursor()
