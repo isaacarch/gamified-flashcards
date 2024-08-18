36ac5aef462e478709afecf93d143ee9ab388205
Isaac C <isaacchris112@gmail.com>
2024-08-18 12:36:23 +0100

    some wip stuff on leaderboard_db, nothing working

--- leaderboard_database.py
+++ leaderboard_database.py
@@ -3,10 +3,49 @@
 import mysql.connector
 import database_login as login
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user= login.username,
-  password=login.password
-)
+# admin credentials
+def adminLogin():
+  mydb = mysql.connector.connect(
+    host="localhost",
+    user= login.username,
+    password=login.password
+  )
+  return mydb.cursor()
 
-mycursor = mydb.cursor()
+# can only SELECT, UPDATE, and INSERT
+def guestLogin():
+  mydb = mysql.connector.connect(
+    host="localhost",
+    user= "guest",
+    password= "\"e$G}bkXs500"
+  )
+  return mydb
+
+def globalBoard(mycursor):
+    command = '''
+CREATE TABLE IF NOT EXISTS global_board
+(
+username VARCHAR(20),
+score INT,
+PRIMARY KEY (username)
+);
+'''
+    mycursor.execute(command)
+
+def fetchUserScore(mycursor, user, db):
+    mycursor.execute(f"SELECT score FROM {db} WHERE username ={user}")
+    return mycursor.fetchall()
+    # returns empty list or [(score)]
+
+def addScore(mycursor, score, db):
+    username = input("Enter your username: ")
+    if fetchUserScore(username, db): # executes if not empty i.e.: user already exists in db
+        command = f"UPDATE {db} SET score ={score} WHERE username ={username}"
+    else:
+        command = f"INSERT INTO {db} (username, score) VALUES ({username}, {score})"
+    mycursor.execute(command)
+
+db = adminLogin()
+cursor = db.cursor()
+globalBoard(cursor)
+db.commit()
