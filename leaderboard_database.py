b4a0d41a5443337417ad8a9d5f35610c0597363b
Isaac C <isaacchris112@gmail.com>
2024-08-18 13:50:22 +0100

    set up db. still not ready

--- leaderboard_database.py
+++ leaderboard_database.py
@@ -8,18 +8,23 @@ def adminLogin():
   mydb = mysql.connector.connect(
     host="localhost",
     user= login.username,
-    password=login.password
+    password=login.password,
+    database = "leaderboards"
   )
-  return mydb.cursor()
+  return mydb
 
 # can only SELECT, UPDATE, and INSERT
 def guestLogin():
   mydb = mysql.connector.connect(
     host="localhost",
     user= "guest",
-    password= "\"e$G}bkXs500"
+    password= "\"e$G}bkXs500",
+    database = "leaderboards"
   )
   return mydb
+
+def createDB(mycursor):
+   mycursor.execute("CREATE DATABASE leaderboards")
 
 def globalBoard(mycursor):
     command = '''
@@ -33,19 +38,21 @@ PRIMARY KEY (username)
     mycursor.execute(command)
 
 def fetchUserScore(mycursor, user, db):
-    mycursor.execute(f"SELECT score FROM {db} WHERE username ={user}")
+    mycursor.execute(f"SELECT score FROM {db} WHERE username =%s", (user,))
     return mycursor.fetchall()
     # returns empty list or [(score)]
 
 def addScore(mycursor, score, db):
     username = input("Enter your username: ")
-    if fetchUserScore(username, db): # executes if not empty i.e.: user already exists in db
-        command = f"UPDATE {db} SET score ={score} WHERE username ={username}"
+    if fetchUserScore(mycursor, username, db): # executes if not empty i.e.: user already exists in db
+        command = f"UPDATE {db} SET score =%s WHERE username =%s"
+        values = (score, username)
     else:
-        command = f"INSERT INTO {db} (username, score) VALUES ({username}, {score})"
-    mycursor.execute(command)
+        command = f"INSERT INTO {db} (username, score) VALUES (%s, %s)"
+        values = (username, score)
+    mycursor.execute(command, values)
 
-db = adminLogin()
+db = guestLogin()
 cursor = db.cursor()
-globalBoard(cursor)
+addScore(cursor, 10, "global_board")
 db.commit()
